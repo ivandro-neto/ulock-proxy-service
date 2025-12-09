ec4c313731fcbffc8459ebf975d8045554b8d179
Ivandro Neto <ivandroneto1@outlook.com>
2025-12-09 11:59:04 +0100

    Add initial project structure with .gitignore, configuration files, and API setup

---
+++ API/Program.cs
@@ -0,0 +1,90 @@
+using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var config = builder.Configuration;
+
+//Add JWT Validation
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = config["Jwt:Issuer"],
+            ValidAudience = config["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Secret"]!)),
+        };
+
+    });
+
+// Add Reverse Proxy
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+
+var app = builder.Build();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.Use(async (context, next) =>
+{
+    var path = context.Request.Path;
+
+    var tokenCookie = context.Request.Cookies["test_token"];
+    var isCallback = path.StartsWithSegments("/auth/callback");
+
+    if (tokenCookie is null && !isCallback)
+    {
+        var returnUrl = Uri.EscapeDataString($"https://{context.Request.Host}{context.Request.Path}{context.Request.QueryString}");
+        context.Response.Redirect($"http://10.11.1.195:5032?returnUrl={returnUrl}");
+        return;
+    }
+
+    if (tokenCookie is not null)
+    {
+        context.Request.Headers.Append("Authorization", $"Bearer {tokenCookie}");
+    }
+    await next();
+});
+
+app.MapGet("/auth/callback", async (context) =>
+{
+    var token = context.Request.Query["token"].ToString();
+    var returnUrl = context.Request.Query["returnUrl"].ToString();
+    if (string.IsNullOrWhiteSpace(token))
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        await context.Response.WriteAsync("Token is missing");
+        return;
+    }
+
+    context.Response.Cookies.Append("test_token", token, new CookieOptions
+    {
+        HttpOnly = true,
+        Secure = true,
+        SameSite = SameSiteMode.Lax,
+        Expires = DateTimeOffset.UtcNow.AddHours(1)
+    });
+
+    context.Response.Redirect(string.IsNullOrWhiteSpace(returnUrl) ? "/" : returnUrl);
+});
+
+app.MapGet("health", () => "API is healthy");
+
+app.MapReverseProxy();
+
+app.Run();
